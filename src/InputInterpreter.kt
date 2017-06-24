8fdeeb00d6e1be9a3f0ae2719da1b3689951a02f
MAKOSA <makosagitsit@anomanode.com>
2017-06-24 13:59:13 -0400

    Small Changes

--- src/InputInterpreter.kt
+++ src/InputInterpreter.kt
@@ -4,21 +4,18 @@ sealed class InputInterpreter {
         fun handle(input: String): Boolean {
             // Check that input length is correct
             if (input.length == 3) {
-                var inputValidated = booleanArrayOf(false, false)
+                val inputValidated = arrayOf(false, false)
                 val validNumericInputs = arrayOf("0", "1", "2")
                 val validSpecialCharacters = arrayOf(":", "|", ",")
 
                 validSpecialCharacters.forEach {
                     // Check for special characters and that they are in the proper location
                     if (input.indexOf(it) == 1) {
-                        // read numeric values sandwiching the special characters and validate input
+                        // read numeric values surrounding the special characters and validate input
                         validNumericInputs.forEach {
                             if (input.substring(0,1).toInt() == it.toInt()) inputValidated.set(0, true)
                             if (input.substring(2,3).toInt() == it.toInt()) inputValidated.set(1, true)
                         }
-
-
-
 
                         // Return true if all inputs are valid
                         return !inputValidated.contains(false)
