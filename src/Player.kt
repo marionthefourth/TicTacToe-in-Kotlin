a2e90d954bce4e8da0b19e8a37838c962adc208e
MAKOSA <makosagitsit@anomanode.com>
2017-06-24 18:50:42 -0400

    Update to Player
    Added mark member variable which is either an “X” or an “O”

--- src/Player.kt
+++ src/Player.kt
@@ -1,14 +1,30 @@
-sealed class Player(name:String) {
+class Player {
 
-    var name = name
     var score = 0
+    var name = ""
+    var mark = " "
 
     constructor(name:String,score:Int) : this(name) {
+
         this.score = score
+    }
+
+    fun getMark(index:Int) : String {
+        if (index == 0) {
+            return "X"
+        } else {
+            return "0"
+        }
     }
 
     fun wonGame() {
         score++
     }
+
+    constructor(name:String) {
+        this.name = name
+    }
+
+    constructor()
 
 }
