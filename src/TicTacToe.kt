7e5f3d5952346a4825b517e65ca9f936ecffb8ab
ParsonsKennethC <parsons.kenneth.c@gmail.com>
2017-06-24 14:58:33 -0400

    InputInterpreter Explicitly Labeled Testing
    lists input along with output

--- src/TicTacToe.kt
+++ src/TicTacToe.kt
@@ -2,7 +2,11 @@ fun main(args: Array<String>) {
     println("Tic-Tac-Toe")
 
     for (i in 0..5) {
-        println(InputInterpreter.handle("$i:$i"))
+        for (j in 0..5) {
+            print("$i:$j ${
+            InputInterpreter.handle("$i:$j")
+            } ")
+        }
+        println()
     }
-
 }
