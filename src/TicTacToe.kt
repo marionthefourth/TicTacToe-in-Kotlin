f5f71d37ca52586878cf71b41bdc0588043483fb
MAKOSA <makosagitsit@anomanode.com>
2017-06-24 18:51:07 -0400

    Additions to TicTacToe
    Added Tests for Previous Classes
    And Starts a Game of TicTacToe

--- src/TicTacToe.kt
+++ src/TicTacToe.kt
@@ -1,11 +1,27 @@
 fun main(args: Array<String>) {
     println("Tic-Tac-Toe")
 
-    for (i in 0..4){
-        for (j in 0..4){
-           print(InputInterpreter.process("$i:$j"))
-           print(" ")
+    GameReferree.newTicTacToeGame(arrayOf(Player(),Player()),Board())
+
+}
+
+fun BoardTests() {
+    val board = Board()
+
+    board.print()
+}
+
+fun InputInterpreterTests() {
+    for (i in 0..4) {
+        for (j in 0..4) {
+            print(InputInterpreter.process("$i:$j",InputInterpreter.InputType.GAMEPLAY))
+            print(" ")
         }
         println()
+
+        for (i in 0..5) {
+            println("${InputInterpreter.canHandle("$i:$i", InputInterpreter.InputType.GAMEPLAY)}")
+            println("i" + 1 + '5')
+        }
     }
 }
