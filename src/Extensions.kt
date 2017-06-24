89151b57f5c19c0605da909295fcc6fa5befddaf
MAKOSA <makosagitsit@anomanode.com>
2017-06-24 18:49:08 -0400

    Extensions for Board 2D Array

---
+++ src/Extensions.kt
@@ -0,0 +1,14 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray
+
+inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int)->INNER): Array<Array<INNER>>
+        = Array(sizeOuter) { Array(sizeInner, innerInit) }
+fun array2dOfInt(sizeOuter: Int, sizeInner: Int): Array<IntArray>
+        = Array(sizeOuter) { IntArray(sizeInner) }
+fun array2dOfLong(sizeOuter: Int, sizeInner: Int): Array<LongArray>
+        = Array(sizeOuter) { LongArray(sizeInner) }
+fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray>
+        = Array(sizeOuter) { ByteArray(sizeInner) }
+fun array2dOfChar(sizeOuter: Int, sizeInner: Int): Array<CharArray>
+        = Array(sizeOuter) { CharArray(sizeInner) }
+fun array2dOfBoolean(sizeOuter: Int, sizeInner: Int): Array<BooleanArray>
+        = Array(sizeOuter) { BooleanArray(sizeInner) }
